846de9a899ec9bedb2e956522a7ffaa782260283
Dhruv-N-Patel <210100109@iitb.ac.in>
2024-05-01 22:47:08 +0530

    last

--- portfolio/portfolio_app/admin.py
+++ portfolio/portfolio_app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Education, Certification, WorkExperience, Project, Blog, ContactForm
+from .models import Education, Certification, WorkExperience, Project, Blog, ContactForm, Skill
 # Register your models here.
 
 admin.site.register(Education)
@@ -8,3 +8,4 @@ admin.site.register(WorkExperience)
 admin.site.register(Project)
 admin.site.register(Blog)
 admin.site.register(ContactForm)
+admin.site.register(Skill)
--- portfolio/portfolio_app/urls.py
+++ portfolio/portfolio_app/urls.py
@@ -5,8 +5,13 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('work/', views.work, name='work'),
     path('content/', views.content, name='content'),
-    path('contact/', views.contact_form_submit, name='contact'),
+    path('contact/', views.contact, name='contact'),
     path("blog_page/<str:pk>/", views.blog_page, name="blog_page"),
     path("contact_form_submit/", views.contact_form_submit, name="contact_form_submit"),
-    path('Projects/',views.projects, name = 'Projects')
-]
+    path('Projects/',views.projects, name = 'Projects'),
+    path('download-resume/', views.download_resume, name='download_resume'),
+    path("resume_contact/", views.resume_contact, name="resume_contact"),
+    path('download_resume_adv/', views.download_resume_adv, name='download_resume_adv'),
+    path('image/', views.work, name='work'),
+    path('bosch/', views.bosch, name='bosch')
+    ] 
